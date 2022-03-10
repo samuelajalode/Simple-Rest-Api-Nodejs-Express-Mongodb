df6b32dbe14b95599966ab28732de5f6cc1bcec5
Ryan Njoroge <keddelyronjoz@gmail.com>
2022-03-10 07:31:37 +0300

    fix: Changed the routes from 'posts' to 'todos' and fixed cors error

--- source/routes/todos.ts
+++ source/routes/todos.ts
@@ -3,10 +3,10 @@ import controller from '../controllers/todos'
 
 const router = express.Router()
 
-router.get('/posts', controller.getTodos)
-router.get('/posts/:id', controller.getTodo)
-router.patch('/posts/:id', controller.updateTodo)
-router.delete('/posts/:id', controller.deleteTodo)
-router.post('/posts', controller.createTodo)
+router.get('/todos', controller.getTodos)
+router.get('/todos/:id', controller.getTodo)
+router.patch('/todos/:id', controller.updateTodo)
+router.delete('/todos/:id', controller.deleteTodo)
+router.post('/todos', controller.createTodo)
 
 export = router
